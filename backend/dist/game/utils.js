0402871342f1cf234f4682a59bf13b2cb600e886
Jiyoo Noh <jiyoonoh@Jiyoos-MacBook-Air.local>
2026-05-29 15:28:38 +0700

    Remove generated backend files from repository

--- backend/dist/game/utils.js
+++
@@ -1,6 +0,0 @@
-export function clamp(value, min, max) {
-    return Math.min(max, Math.max(min, value));
-}
-export function randomBetween(min, max) {
-    return Math.floor(min + Math.random() * (max - min));
-}
